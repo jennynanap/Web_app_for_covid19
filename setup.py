a799aa343a3444a8b1ed7f93c0cbf6db565f5259
jennynanap <jennynanap30@gmail.com>
2020-03-27 17:49:38 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='build web app by jupyter notebook for covid19 via Vuetify and Voila',
+    author='Jenny',
+    license='MIT',
+)
